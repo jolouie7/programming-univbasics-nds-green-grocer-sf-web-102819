da2a9766d2f9ca5b8b93048f85cf4b4403e8511f
Joseph  Louie <joseph161216@gmail.com>
2019-10-27 04:58:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -94,7 +94,7 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  cart[0].each do |item|
+  cart.each do |item|
     #check if item is on clearance
     if item[:clearance] == true
       item[:price] = item[:price] * .20
